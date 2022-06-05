8fb9f6e6945b33fcfa9c9eda35c6749c77a00223
Cosmin-Yuriy <102223557+Cosmin-Yuriy@users.noreply.github.com>
2022-06-06 00:29:49 +0300

    Исправил

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,10 @@ const Title = document.querySelector('.profile__title')
 const SubTitle = document.querySelector('.profile__subtitle')
 const popup = document.querySelector('.popup')
 const buttonEdit = document.querySelector('.profile__button-edit')
-const editTitle = document.querySelector('.popup__text')
+const editTitle = document.querySelector('.popup__text[name="name"]')
 const editSubTitle = document.querySelector('.popup__text[name="subtitle"]')
-console.log(editSubTitle)
 const popupForm = document.querySelector('.popup__form')
-// Из-за добавления type="button" меняем немного JS, я бы добавил TYPE="Submit", 
-// так как мы будем всё таки это оправлять, но сказано - сделано:
-let ButtonSave = document.getElementsByTagName("button")[8];
-// console.log(ButtonSave);
+
 
 //Включаем кнопку, дословно добавляем к классу popup + класс popup-Open
 buttonEdit.addEventListener('click', function(){
@@ -27,9 +23,8 @@ function closePopup(){
 //Закрываем наш попап крестиком без сохранения с помощью функции
 popupCloseButton.addEventListener('click', closePopup);
 
-//ПРИШЛОСЬ ПОМЕНЯТЬ / теперь работает если только на кнопку нажать, на ENTER не работает
-//Здесь при нажатие кнопки сохранить мышкой и enter закроем и сохраним
-ButtonSave.addEventListener('click', function(event){
+//Здесь при нажатие кнопки сохранить мышкой или enter закроем и сохраним
+popupForm.addEventListener('submit', function(event){ 
   console.log (event);
   event.preventDefault();
   Title.textContent = editTitle.value;
