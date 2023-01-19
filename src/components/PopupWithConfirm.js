889dbca3701331d585cdaae5f8723b340f1e00b2
Cosmin-Yuriy <102223557+Cosmin-Yuriy@users.noreply.github.com>
2023-01-19 20:06:46 +0300

    PR 9

--- src/components/PopupWithConfirm.js
+++ src/components/PopupWithConfirm.js
@@ -1,34 +1,34 @@
-import Popup from "./Popup.js";
+// import Popup from "./Popup.js";
 
-class PopupWithConfirm extends Popup {
-  constructor(popupElement){
-    super(popupElement);
-   // this._calbacklSubmitForm = calbacklSubmitForm;
-  }
+// class PopupWithConfirm extends Popup {
+//   constructor(popupElement){
+//     super(popupElement);
+//    // this._calbacklSubmitForm = calbacklSubmitForm;
+//   }
 
 
-// setEventListeners() {
-//   super.setEventListeners();
-//   this._buttonSubmitPopup.addEventListener('click', () => {
-//     this._calbacklSubmitForm
-//   })
-//}
-// setEventListeners() {
-//   //Перезаписывает родительский метод setEventListeners.
-//   super.setEventListeners();
-//   //ставит слушатель на форме, на кнопку (button) и если нажать, то будет следущее:
-//   this._form.addEventListener("submit", (evt) => {
-//     evt.preventDefault();
-//     this._calbacklSubmitForm(this._getInputValues());
-//   });
+// // setEventListeners() {
+// //   super.setEventListeners();
+// //   this._buttonSubmitPopup.addEventListener('click', () => {
+// //     this._calbacklSubmitForm
+// //   })
+// //}
+// // setEventListeners() {
+// //   //Перезаписывает родительский метод setEventListeners.
+// //   super.setEventListeners();
+// //   //ставит слушатель на форме, на кнопку (button) и если нажать, то будет следущее:
+// //   this._form.addEventListener("submit", (evt) => {
+// //     evt.preventDefault();
+// //     this._calbacklSubmitForm(this._getInputValues());
+// //   });
+// // }
+
+
+
+// popupWithConfirm(calbacklSubmitForm) {
+//   this._calbacklSubmitForm = calbacklSubmitForm;
 // }
 
+// }
 
-
-popupWithConfirm(calbacklSubmitForm) {
-  this._calbacklSubmitForm = calbacklSubmitForm;
-}
-
-}
-
-export default PopupWithConfirm;
+// export default PopupWithConfirm;
